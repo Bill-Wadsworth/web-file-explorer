d0fe15bf4b243bd6826825528bcd257d3153c185
Bill Wadsworth <ww2g23@soton.ac.uk>
2024-08-06 16:23:01 +0100

    working browsing of file structure using api

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/files/", (request, response) => {
-  response.status(200).send(getFileJson(""));
+  console.log(generateTotalFileJson());
+  response.status(200).send(generateTotalFileJson());
 });
-
-app.get("/api/files/:path", (request, response) => {
+//Do not ask how the file regex works, it just does.
+app.get("/api/files/:path(([a-zA-Z]+)*)", (request, response) => {
+  console.log(request.params);
   const filePath = request.params.path;
   response.status(200).send(getFileJson(filePath));
 });
@@ -22,6 +24,29 @@ app.listen(PORT, () => {
 
 //FILE MANAGMENT SECTION
 
+const STORAGEPATH = "./storage";
+
 const getFileJson = (path) => {
-  return { test: { type: "file" }, test2: { type: "file", children: {} } };
+  return generateTotalFileJson(path);
+};
+
+const generateTotalFileJson = (path = "") => {
+  var absolutePath = STORAGEPATH + "/" + path;
+  const fs = require("fs");
+  var currentFile = {};
+  var files = fs.readdirSync(absolutePath, { withFileTypes: true });
+  files.forEach((file) => {
+    var filePath = path + "/" + file.name;
+    const fileInfo = fs.statSync(STORAGEPATH + "/" + filePath);
+    if (fileInfo.isDirectory()) {
+      currentFile[file.name] = {
+        path: filePath,
+        type: "directory",
+        children: generateTotalFileJson(filePath),
+      };
+    } else {
+      currentFile[file.name] = { type: "file" };
+    }
+  });
+  return currentFile;
 };
